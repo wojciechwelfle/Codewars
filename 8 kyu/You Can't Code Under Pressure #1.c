a3fe32c5c61202c5f53054783e853f3edb8726eb
Wafelek <111795716+wojciechwelfle@users.noreply.github.com>
2023-04-06 15:44:46 +0200

    You Can't Code Under Pressure #1

---
+++ 8 kyu/You Can't Code Under Pressure #1.c
@@ -0,0 +1,10 @@
+/*
+    Description:
+    Code as fast as you can! You need to double the integer and return it.
+*/
+
+#include <stdint.h>
+
+int32_t double_integer(int32_t val) {
+    return 2 * val;
+}
