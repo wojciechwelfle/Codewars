11871284208197d37f3f3b1afe37b4b62b874423
Wafelek <111795716+wojciechwelfle@users.noreply.github.com>
2023-04-07 10:39:50 +0200

    Find out whether the shape is a cube

---
+++ 8 kyu/Find out whether the shape is a cube.py
@@ -0,0 +1,26 @@
+"""
+    Description:
+    To find the volume (centimeters cubed) of a cuboid you use the formula:
+
+    volume = Length * Width * Height
+
+    But someone forgot to use proper record keeping, so we only have the volume, and the length of a single side!
+
+    It's up to you to find out whether the cuboid has equal sides (= is a cube).
+
+    Return true if the cuboid could have equal sides, return false otherwise.
+
+    Return false for invalid numbers too (e.g volume or side is less than or equal to 0).
+
+    Note: side will be an integer
+"""
+
+import math
+
+
+def cube_checker(volume, side):
+    if volume <= 0 or side <= 0:
+        return False
+    if math.sqrt(volume / side) - int(math.sqrt(volume / side)) == 0:
+        return True
+    return False
