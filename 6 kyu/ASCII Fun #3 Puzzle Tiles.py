b96f63bf3b907ad8ebcb106157260a0a9d248c12
Wafelek <111795716+wojciechwelfle@users.noreply.github.com>
2023-04-07 09:57:22 +0200

    ASCII Fun #3: Puzzle Tiles

--- 6 kyu/ASCII Fun #3 Puzzle Tiles.py
+++ 6 kyu/ASCII Fun #3 Puzzle Tiles.py
@@ -1,3 +1,29 @@
+"""
+    Description:
+    Because my other two parts of the serie were pretty well received I decided to do another part.
+
+    Puzzle Tiles
+    You will get two Integer n(width) and m (height) and your task is to draw following pattern. Each line is seperated with '\n'.
+
+    Both integers are equal or greater than 1. No need to check for invalid parameters.
+    There are no whitespaces at the end of each line.
+    For example, for width = 4 and height = 3, you should draw the following pattern:
+
+       _( )__ _( )__ _( )__ _( )__
+     _|     _|     _|     _|     _|
+    (_   _ (_   _ (_   _ (_   _ (_
+     |__( )_|__( )_|__( )_|__( )_|
+     |_     |_     |_     |_     |_
+      _) _   _) _   _) _   _) _   _)
+     |__( )_|__( )_|__( )_|__( )_|
+     _|     _|     _|     _|     _|
+    (_   _ (_   _ (_   _ (_   _ (_
+     |__( )_|__( )_|__( )_|__( )_|
+
+    For more informations take a look in the test cases!
+"""
+
+
 def puzzle_tiles(width, height):
     puzzle = '  '
     for _ in range(width):
