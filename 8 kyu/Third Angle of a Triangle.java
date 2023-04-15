021bde80b6dfbddb2b38f7ee7aeaa3055fb52d2e
wojciechwelfle <wojciech.welfle@gmail.com>
2023-04-15 23:49:42 +0200

    Third Angle of a Triangle

--- 8 kyu/Third Angle of a Triangle.java
+++ 8 kyu/Third Angle of a Triangle.java
@@ -8,7 +8,7 @@
     https://en.wikipedia.org/wiki/Triangle
 */
 
-public class ThirdAngle {
+class ThirdAngle {
     public static int otherAngle(int angle1, int angle2) {
         return 180 - angle1 - angle2;
     }
