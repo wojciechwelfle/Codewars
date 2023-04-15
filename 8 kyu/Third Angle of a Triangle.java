459d238f419d371f5d8f119a94c10091853651dc
wojciechwelfle <wojciech.welfle@gmail.com>
2023-04-15 23:49:05 +0200

    Third Angle of a Triangle

---
+++ 8 kyu/Third Angle of a Triangle.java
@@ -0,0 +1,15 @@
+/*
+    You are given two interior angles (in degrees) of a triangle.
+
+    Write a function to return the 3rd.
+
+    Note: only positive integers will be tested.
+
+    https://en.wikipedia.org/wiki/Triangle
+*/
+
+public class ThirdAngle {
+    public static int otherAngle(int angle1, int angle2) {
+        return 180 - angle1 - angle2;
+    }
+}
