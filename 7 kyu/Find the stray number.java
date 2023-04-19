5559b23adb310f58fb045cd6442e4458cfc9d9c2
wojciechwelfle <wojciech.welfle@gmail.com>
2023-04-19 11:50:35 +0200

    Find the stray number

---
+++ 7 kyu/Find the stray number.java
@@ -0,0 +1,20 @@
+/*
+    Description:
+    You are given an odd-length array of integers, in which all of them are the same, except for one single number.
+
+    Complete the method which accepts such an array, and returns that single different number.
+
+    The input array will always be valid! (odd-length >= 3)
+
+    Examples
+    [1, 1, 2] ==> 2
+    [17, 17, 3, 17, 17, 17, 17] ==> 3
+*/
+
+
+class Solutionn {
+    static int stray(int[] numbers) {
+        java.util.Arrays.sort(numbers);
+        return numbers[0] != numbers[1] ? numbers[0] : numbers[numbers.length - 1];
+    }
+}
