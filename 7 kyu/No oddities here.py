d6836cd1b7bf1bdbb0a1b642d45fb128d17ce8cb
wojciechwelfle <wojciech.welfle@gmail.com>
2023-04-19 16:05:58 +0200

    No oddities here

---
+++ 7 kyu/No oddities here.py
@@ -0,0 +1,11 @@
+"""
+Description:
+
+Write a small function that returns the values of an array that are not odd.
+
+All values in the array will be integers. Return the good values in the order they are given.
+"""
+
+
+def no_odds(values):
+    return [i for i in values if i % 2 == 0]
