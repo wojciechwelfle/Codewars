9d831b06fe1160868ec533a1f4587e04953152bb
wojciechwelfle <wojciech.welfle@gmail.com>
2023-04-19 15:44:19 +0200

    Power of two

--- 7 kyu/Power of two.java
+++ 7 kyu/Power of two.java
@@ -18,8 +18,9 @@
 
 class PowerOfTwo {
     public static boolean isPowerOfTwo(long n) {
+        if (n == 1) return true;
         while (n != 2) {
-            if (n % 2 != 0 || n == 0) return false;
+            if (n % 2 != 0 || n < 2) return false;
             n /= 2;
         }
         return true;
