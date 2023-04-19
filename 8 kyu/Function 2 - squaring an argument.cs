d7399d77a50b1564957770412211d1ecd2680feb
wojciechwelfle <wojciech.welfle@gmail.com>
2023-04-19 17:56:58 +0200

    Function 2 - squaring an argument

---
+++ 8 kyu/Function 2 - squaring an argument.cs
@@ -0,0 +1,12 @@
+/*
+    Description:
+    Now you have to write a function that takes an argument and returns the square of it.
+*/
+
+public class Kata
+{
+    public static double Square(double val)
+    {
+        return val * val;
+    }
+}
