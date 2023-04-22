d4002711f579c62029751da8a8110dbb56e05aaa
wojciechwelfle <wojciech.welfle@gmail.com>
2023-04-23 00:25:31 +0200

    Sum Factorial

---
+++ 7 kyu/Sum Factorial.c
@@ -0,0 +1,31 @@
+/*
+    Description:
+
+    Factorials are often used in probability and are used as an introductory problem for looping constructs.
+    In this kata you will be summing together multiple factorials.
+
+    Here are a few examples of factorials:
+
+    4 Factorial = 4! = 4 * 3 * 2 * 1 = 24
+
+    6 Factorial = 6! = 6 * 5 * 4 * 3 * 2 * 1 = 720
+    In this kata you will be given a list of values that you must first find the factorial, and then return their sum.
+
+    For example if you are passed the list [4, 6] the equivalent mathematical expression would be 4! + 6!
+    which would equal 744.
+
+    Good Luck!
+
+    Note: Assume that all values in the list are positive integer values > 0 and each value in the list is unique.
+*/
+
+
+#include <stddef.h>
+
+unsigned long long factorial(const int val) {
+   return val ? val * factorial(val - 1) : 1;
+}
+
+unsigned long long sum_factorial(size_t z, const int array[z]) {
+   return z ? factorial(array[z - 1]) + sum_factorial(z - 1, array) : 0;
+}
